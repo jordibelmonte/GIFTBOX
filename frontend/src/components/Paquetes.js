3d331b0830f53462d0c4ede1bb1b3d07d6711e47
Juan Carlos Rabellino <juancarabellino@gmail.com>
2021-03-11 02:02:45 -0300

    Filtro múltiple

--- frontend/src/components/Paquetes.js
+++ frontend/src/components/Paquetes.js
@@ -8,9 +8,6 @@ import { BiSearch } from 'react-icons/bi'
 const Paquetes = ({ paquetesFiltrados, filtrarPaquetes, location, todosLosPaquetes, todasLasCategorias }) => {
   const [valor, setValor] = useState(false)
   const [categoria, setCategoria] = useState(true)
-  const selects = [document.getElementById('select1'), document.getElementById('select2'), document.getElementById('select3'), document.getElementById('select4')]
-  console.log(selects)
-  // const [paquetes, setPaquetes] = useState([])
   const ciudades = ["Buenos Aires", "Santa Fe", "Córdoba"]
 
   var paquetes = []
@@ -32,31 +29,30 @@ const Paquetes = ({ paquetesFiltrados, filtrarPaquetes, location, todosLosPaquet
     filtrarPaquetes(location.categoria)
     setCategoria(false)
   }
-  todasLasCategorias && console.log(todasLasCategorias)
-  paquetesFiltrados && console.log(paquetesFiltrados)
+
   return (
     <main className='packagesMain'>
       <input type="text" placeholder="Buscar por nombre" onChange={buscando} />
       <div className="filterInput">
 
-        <select id='select1' name="categoria" disabled={true} onChange={buscando}>
+        <select name="categoria" onChange={buscando}>
           <option value="">Todos las categorías</option>
           {todasLasCategorias && todasLasCategorias.map(categoria => <option value={categoria.nombre}>{categoria.nombre}</option>)}
         </select>
 
-        <select id='select2' name="precio" onChange={buscando}>
+        <select name="precio" onChange={buscando}>
           <option className="option" value="">Precios</option>
           <option value="2000">Menos de $2000</option>
           <option value="5000">Menos de $5000</option>
           <option value="10000">Menos de $10000</option>
         </select>
 
-        <select id='select3' name="cantidadPersonas" onChange={buscando}>
+        <select name="cantidadPersonas" onChange={buscando}>
           <option value="">Cantidad de personas</option>
           {[...Array(4)].map((m, i) => <option value={i + 1}>{i + 1}</option>)}
         </select>
 
-        <select id='select4' name="ubicacion" onChange={buscando}>
+        <select name="ubicacion" onChange={buscando}>
           <option value="">Ubicación</option>
           {ciudades.map(ciudad => <option value={ciudad}>{ciudad}</option>)}
         </select>
